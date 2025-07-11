a32c0ebf0ef1a514dea96617a8b192bb25c6c8a8
marcosvarela5 <marcosviesmugardos@gmail.com>
2025-07-11 03:28:55 +0200

    [FUNC-O] Domain Socio repository implementation

--- backend/src/main/java/es/marcos/backend/domain/repository/SocioRepository.java
+++ backend/src/main/java/es/marcos/backend/domain/repository/SocioRepository.java
@@ -1,4 +1,33 @@
 package es.marcos.backend.domain.repository;
 
-public class SocioRepository {
+import es.marcos.backend.domain.enums.SocioState;
+import es.marcos.backend.domain.model.Socio;
+import es.marcos.backend.domain.model.SocioCriteria;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SocioRepository {
+
+    void save(Socio socio);
+
+    List<Socio> findAll(Pageable pageable);
+
+    Optional<Socio> findById(Long id);
+
+    Optional<Socio> findByName(String name);
+
+    void delete(Socio socio);
+
+    void deleteById(Long id);
+
+    List<Socio> findByState(SocioState state);
+
+    boolean existsById(Long id);
+
+    boolean existsByName(String name);
+
+    Page<Socio> findByCriteria(SocioCriteria criteria, Pageable pageable);
 }
