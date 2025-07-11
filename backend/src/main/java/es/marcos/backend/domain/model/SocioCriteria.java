92955aef5b5f88e7e6edf1700f093b5fede2c0f0
marcosvarela5 <marcosviesmugardos@gmail.com>
2025-07-11 03:28:38 +0200

    [FUNC-O] Basic entities implementation

---
+++ backend/src/main/java/es/marcos/backend/domain/model/SocioCriteria.java
@@ -0,0 +1,10 @@
+package es.marcos.backend.domain.model;
+
+public class SocioCriteria {
+
+    private String name;
+    private String surname;
+    private boolean imageConsent;
+    private boolean acceptsPayment;
+    private boolean receivesNotifications;
+}
