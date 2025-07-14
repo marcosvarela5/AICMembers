05a7ccb656fab00ffe4dee7f2fafb16eabfa363e
marcosvarela5 <marcosviesmugardos@gmail.com>
2025-07-14 03:19:42 +0200

    [OTHER] Security config started

--- backend/src/main/java/es/marcos/backend/domain/exception/UserNotFoundException.java
+++ backend/src/main/java/es/marcos/backend/domain/exception/UserNotFoundException.java
@@ -1,6 +1,10 @@
 package es.marcos.backend.domain.exception;
 
 public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(Long id) {
+        super("No se encontró el usuario con id " + id);
+    }
+
     public UserNotFoundException(String message) {
         super(message);
     }
