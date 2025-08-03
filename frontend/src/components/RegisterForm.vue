7eade30972d3eae11ef2550536d1539441c93fb2
marcosvarela5 <marcosviesmugardos@gmail.com>
2025-08-04 01:54:54 +0200

    [FUNC-0] Fixed bug with validation errors

--- frontend/src/components/RegisterForm.vue
+++ frontend/src/components/RegisterForm.vue
@@ -1,29 +1,33 @@
 <template>
   <div class="register-form">
     <div class="card">
-      <img :src="aicLogo" alt="AIC logo" class="logo" />
+      <img :src="aicLogo" alt="AIC logo" class="logo"/>
 
       <form class="form" @submit.prevent="handleSubmit">
         <div class="form-grid">
           <!-- Datos persoais -->
           <div class="section">
             <label for="name">Nome:
-              <input id="name" type="text" v-model="form.name" @input="validateField('name')" class="input" placeholder="Escribe o teu nome"/>
+              <input id="name" type="text" v-model="form.name" @input="validateField('name')" class="input"
+                     placeholder="Escribe o teu nome"/>
               <span class="error" v-if="errors.name">{{ errors.name }}</span>
             </label>
 
             <label for="surname">Apelidos:
-              <input id="surname" type="text" v-model="form.surname" @input="validateField('surname')" class="input" placeholder="Escribe os teus apelidos"/>
+              <input id="surname" type="text" v-model="form.surname" @input="validateField('surname')" class="input"
+                     placeholder="Escribe os teus apelidos"/>
               <span class="error" v-if="errors.surname">{{ errors.surname }}</span>
             </label>
 
             <label for="dni">DNI:
-              <input id="dni" type="text" v-model="form.dni" @input="validateField('dni')" class="input" placeholder="12345678X"/>
+              <input id="dni" type="text" v-model="form.dni" @input="validateField('dni')" class="input"
+                     placeholder="12345678X"/>
               <span class="error" v-if="errors.dni">{{ errors.dni }}</span>
             </label>
 
             <label for="birthDate">Data de nacemento:
-              <input id="birthDate" type="date" v-model="form.birthDate" @input="validateField('birthDate')" class="input"/>
+              <input id="birthDate" type="date" v-model="form.birthDate" @input="validateField('birthDate')"
+                     class="input"/>
               <span class="error" v-if="errors.birthDate">{{ errors.birthDate }}</span>
             </label>
           </div>
@@ -31,22 +35,26 @@
           <!-- Dirección -->
           <div class="section">
             <label for="address">Enderezo:
-              <input id="address" type="text" v-model="form.address" @input="validateField('address')" class="input" placeholder="Rúa, avenida, etc."/>
+              <input id="address" type="text" v-model="form.address" @input="validateField('address')" class="input"
+                     placeholder="Rúa, avenida, etc."/>
               <span class="error" v-if="errors.address">{{ errors.address }}</span>
             </label>
 
             <label for="zipCode">Código Postal:
-              <input id="zipCode" type="text" v-model="form.zipCode" @input="validateField('zipCode')" class="input" placeholder="XXXXX"/>
+              <input id="zipCode" type="text" v-model="form.zipCode" @input="validateField('zipCode')" class="input"
+                     placeholder="XXXXX"/>
               <span class="error" v-if="errors.zipCode">{{ errors.zipCode }}</span>
             </label>
 
             <label for="town">Localidade:
-              <input id="town" type="text" v-model="form.town" @input="validateField('town')" class="input" placeholder="Cidade ou vila"/>
+              <input id="town" type="text" v-model="form.town" @input="validateField('town')" class="input"
+                     placeholder="Cidade ou vila"/>
               <span class="error" v-if="errors.town">{{ errors.town }}</span>
             </label>
 
             <label for="province">Provincia:
-              <input id="province" type="text" v-model="form.province" @input="validateField('province')" class="input" placeholder="Provincia"/>
+              <input id="province" type="text" v-model="form.province" @input="validateField('province')" class="input"
+                     placeholder="Provincia"/>
               <span class="error" v-if="errors.province">{{ errors.province }}</span>
             </label>
           </div>
@@ -54,12 +62,14 @@
           <!-- Contacto -->
           <div class="section">
             <label for="telephone">Teléfono:
-              <input id="telephone" type="tel" v-model="form.telephone" @input="validateField('telephone')" class="input" placeholder="Número fixo"/>
+              <input id="telephone" type="tel" v-model="form.telephone" @input="validateField('telephone')"
+                     class="input" placeholder="Número fixo"/>
               <span class="error" v-if="errors.telephone">{{ errors.telephone }}</span>
             </label>
 
             <label for="mobilePhone">Teléfono móbil:
-              <input id="mobilePhone" type="tel" v-model="form.mobilePhone" @input="validateField('mobilePhone')" class="input" placeholder="Número móbil"/>
+              <input id="mobilePhone" type="tel" v-model="form.mobilePhone" @input="validateField('mobilePhone')"
+                     class="input" placeholder="Número móbil"/>
               <span class="error" v-if="errors.mobilePhone">{{ errors.mobilePhone }}</span>
             </label>
           </div>
@@ -67,12 +77,14 @@
           <!-- Login -->
           <div class="section">
             <label for="email">Email:
-              <input id="email" type="email" v-model="form.email" @input="validateField('email')" class="input" placeholder="usuario@exemplo.com"/>
+              <input id="email" type="email" v-model="form.email" @input="validateField('email')" class="input"
+                     placeholder="usuario@exemplo.com"/>
               <span class="error" v-if="errors.email">{{ errors.email }}</span>
             </label>
 
             <label for="password">Contrasinal:
-              <input id="password" type="password" v-model="form.password" @input="validateField('password')" class="input" placeholder="********"/>
+              <input id="password" type="password" v-model="form.password" @input="validateField('password')"
+                     class="input" placeholder="********"/>
               <span class="error" v-if="errors.password">{{ errors.password }}</span>
             </label>
           </div>
@@ -80,12 +92,14 @@
           <!-- Datos bancarios -->
           <div class="section">
             <label for="bankName">Entidade bancaria:
-              <input id="bankName" type="text" v-model="form.bankName" @input="validateField('bankName')" class="input" placeholder="Nome do banco"/>
+              <input id="bankName" type="text" v-model="form.bankName" @input="validateField('bankName')" class="input"
+                     placeholder="Nome do banco"/>
               <span class="error" v-if="errors.bankName">{{ errors.bankName }}</span>
             </label>
 
             <label for="iban">IBAN:
-              <input id="iban" type="text" v-model="form.iban" @input="validateField('iban')" class="input" placeholder="ES00 0000 0000 0000 0000 0000"/>
+              <input id="iban" type="text" v-model="form.iban" @input="validateField('iban')" class="input"
+                     placeholder="ES00 0000 0000 0000 0000 0000"/>
               <span class="error" v-if="errors.iban">{{ errors.iban }}</span>
             </label>
           </div>
@@ -124,8 +138,6 @@
     </div>
   </div>
 </template>
-
-
 
 
 <script setup lang="ts">
@@ -171,14 +183,22 @@ function validateField(field: string) {
       errors.province = form.province === '' ? 'A provincia é obrigatoria.' : '';
       break
     case 'telephone':
-      errors.telephone = form.telephone === '' ? '' : (/^[0-9]+$/.test(form.telephone) ? '' : 'O teléfono debe ser numérico.');
-      break
+      errors.telephone = form.telephone.trim() === '' || /^[0-9]{7,15}$/.test(form.telephone) ? '' : 'O teléfono debe ser numérico.';
+      break;
     case 'mobilePhone':
-      errors.mobilePhone = form.mobilePhone === '' ? '' : (/^[0-9]+$/.test(form.mobilePhone) ? '' : 'O teléfono debe ser numérico.');
-      break
+      errors.mobilePhone = form.mobilePhone.trim() === '' || /^[0-9]{7,15}$/.test(form.mobilePhone) ? '' : 'O teléfono debe ser numérico.';
+      break;
+
     case 'email':
-      errors.email = form.email === '' ? '' : (/^[0-9]+$/.test(form.email) ? '' : 'O email non é correcto.');
-      break
+      if (form.email.trim() === '') {
+        errors.email = '';
+      } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(form.email)) {
+        errors.email = 'O email non é correcto.';
+      } else {
+        errors.email = '';
+      }
+      break;
+
     case 'password':
       errors.password = form.password.length >= 6 ? '' : 'O contrasinal debe ter 6 caracteres mínimo.';
       break
@@ -186,8 +206,15 @@ function validateField(field: string) {
       errors.bankName = form.bankName.trim() ? '' : 'A entidade bancaria é obrigatoria.';
       break
     case 'iban':
-      errors.iban = form.iban === '' ? '' : (/^[0-9]+$/.test(form.iban) ? '' : 'O IBAN non é correcto');
-      break
+      if (form.iban.trim() === '') {
+        errors.iban = '';
+      } else if (!/^([A-Z]{2}\d{2}\s?\d{4}\s?\d{4}\s?\d{4}\s?\d{4}|\d+)$/.test(form.iban.replace(/\s/g, ''))) {
+        errors.iban = 'O IBAN non é correcto.';
+      } else {
+        errors.iban = '';
+      }
+      break;
+
     case 'receivesNotifications':
       errors.receivesNotifications = form.receivesNotifications ? '' : 'Este campo é obrigatorio.';
       break
